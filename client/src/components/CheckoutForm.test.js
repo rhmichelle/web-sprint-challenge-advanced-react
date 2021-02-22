3dda6683535e2ad27b2327fd10ec49014da629a2
michelleRH <dev@michellerh.com>
2021-02-22 01:44:05 -0500

    First test working

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("app runs", () => {
+    render(<CheckoutForm />);
+})
 
-test("form shows success message on submit with form details", () => {});
+
+test("form header renders", () => {
+// Arrange: Render Checkout Form component
+    render(<CheckoutForm />)
+// Act : get our header DOM
+    const header = screen.getByText("Checkout Form")
+    console.log(header);
+// Assert
+});
+
+// test("form shows success message on submit with form details", () => {});
